25534f51e144fff6cca85caf33103bb483f53c91
harshika.malhotra <malhotra.harshikaa@gmail.com>
2025-05-10 09:39:50 +0530

    Create lc-checkIfArrayIsSortedAndRotated.cpp

---
+++ lc-checkIfArrayIsSortedAndRotated.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    void descending(vector<int> &ar)
+    {
+        int n = ar.size();
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (ar[j] < ar[j + 1])
+                {
+                    swap(ar[j], ar[j + 1]);
+                }
+            }
+        }
+    }
+
+    void reverseRange(vector<int> &ar, int P1, int P2)
+    {
+        while (P1 < P2)
+        {
+            swap(ar[P1], ar[P2]);
+            P1++;
+            P2--;
+        }
+        // return ar;
+    }
+
+    bool check(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            vector<int> org = nums;
+            descending(org);
+            vector<int> rev = org;
+            reverseRange(rev, 0, i);
+            reverseRange(rev, i + 1, n - 1);
+            if (rev == nums)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
