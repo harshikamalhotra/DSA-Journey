95550013ddc1a8003f8da59811761481910ab850
harshika.malhotra <malhotra.harshikaa@gmail.com>
2025-07-16 17:43:19 +0530

    Create gfg-sumOfSubarrays.cpp

---
+++ gfg-sumOfSubarrays.cpp
@@ -0,0 +1,15 @@
+// User function template for C++
+
+class Solution {
+  public:
+    long long subarraySum(vector<int>& arr) {
+        // Your code goes here
+        long long n = arr.size();
+        long long sum = 0;
+        for(long long i=0; i<n; ++i){
+            long long occ = (long long)(i+1)*(long long)(n-i);
+            sum += (long long)arr[i] * occ;
+        }
+        return sum% 1000000007;
+    }
+};
